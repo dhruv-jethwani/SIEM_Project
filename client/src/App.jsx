01e20d33e31b5e88ed3c0570b8472da14d137467
Dhruv Jethwani <dhruvjethwani56@gmail.com>
2025-11-02 20:48:41 +0530

    Initial frontend to work with

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>My React app</h1>
+    </div>
+  )
+}
+
+export default App
